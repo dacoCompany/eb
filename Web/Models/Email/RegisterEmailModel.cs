5c131effbc3d640dff11420bc957510e636f2db9
Martin Svoboda <ovb.svoboda@gmail.com>
2017-10-12 19:33:53 +0200

    Added base template for email

---
+++ Web/Models/Email/RegisterEmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.eBado.Models.Email
+{
+    public class RegisterEmailModel
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
