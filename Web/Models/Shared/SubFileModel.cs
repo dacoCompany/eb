b499c25f319006b4d6f6b9a5cf067b5a5a393fd1
Gergő Marci <gergo.marci1@gmail.com>
2018-01-10 15:27:11 +0100

    fixed fileupload failing

---
+++ Web/Models/Shared/SubFileModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.eBado.Models.Shared
+{
+    public class SubFileModel
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "size")]
+        public int Size { get; set; }
+
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+
+        [JsonProperty(PropertyName = "deleteUrl")]
+        public string DeleteUrl { get; set; }
+
+        [JsonProperty(PropertyName = "thumbnailUrl")]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonProperty(PropertyName = "deleteType")]
+        public string DeleteType { get; set; }
+    }
+}
