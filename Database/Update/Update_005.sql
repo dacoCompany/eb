eb8dcf706d567385945930c7019d5e44fc501ce2
Gergő Marci <gergo.marci1@gmail.com>
2018-01-30 12:20:57 +0100

    added table for email templates

---
+++ Database/Update/Update_005.sql
@@ -0,0 +1,21 @@
+﻿BEGIN;
+		PRINT 'Creating Table [EmailTemplate]'
+		CREATE TABLE [dbo].[EmailTemplate]
+		(
+			[Id] INT IDENTITY(1,1) NOT NULL,
+			[Type] VARCHAR(30) NOT NULL,
+			[SkSubject] VARCHAR(50),
+			[SkBody] NVARCHAR,
+			[CzSubject] VARCHAR(50),
+			[CzBody] NVARCHAR,
+			[HuSubject] VARCHAR(50),
+			[HuBody] NVARCHAR,
+			[EnSubject] VARCHAR(50),
+			[EnBody] NVARCHAR,
+			[Is_Active] BIT NOT NULL DEFAULT 'true',
+			[Date_Created] DATETIME2(0) DEFAULT CURRENT_TIMESTAMP,
+			[Date_Modified] DATETIME2(2) DEFAULT CURRENT_TIMESTAMP,
+			CONSTRAINT PK_EMAIL_TEMPLATE_ID PRIMARY KEY(Id) 
+		)
+		PRINT 'Table created successfully'
+		END 
